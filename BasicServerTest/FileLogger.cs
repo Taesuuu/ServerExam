1b6adeefe9ce8b7e1fb41811fb8a6638cf1699a0
Taesuuu <82026757+Taesuuu@users.noreply.github.com>
2022-06-17 23:50:56 +0900

    Exam

---
+++ BasicServerTest/FileLogger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+// 201727026_정태수
+namespace FinalTestSample
+{
+    public class FileLogger
+    {
+        public string _fileName { get; set; }
+
+        public string logPath = null;
+        public FileStream fsa = null;
+
+        public FileLogger(string fileName)
+        {
+            //문제 6:
+            //_fileName에 filename + 날짜.log 로 로그파일명 저장
+            //예제 > fileName이 Log일때 Log220616.log
+            //_fileName = Log220616.log
+            // _fileName = fileName + DateTime.Now.ToString("yyMMdd") + ".log";
+            string savePath = @fileName + DateTime.Now.ToString("yyMMdd") + ".log";
+            logPath = savePath;
+            if (!File.Exists(savePath))
+            {
+                using (fsa = File.Create(savePath))
+                {
+                    Console.WriteLine("파일 생성 성공");
+                    fsa.Close();
+                }
+            }
+            else
+            {
+                Console.WriteLine("이미 파일이 존재 합니다.");
+            }
+            
+        }
+
+        public void Write(string logType, string logMessage)
+        {
+            //문제 7:
+            //로그는 [날짜 시간] <logType> logMessage로 작성되도록 아래 log라는 string에 저장
+            //예시 > [2022-06-16 12:00:00] <Error> 로그인 에러
+            //fsa = File.Open(logPath, FileMode.Open);
+            string logTime = DateTime.Now.ToString("[yyyy-MM-dd hh:mm:ss]");
+
+            string logText = logTime + " <" + logType +"> " + logMessage + '\n';
+            System.IO.File.AppendAllText(logPath, logText, Encoding.UTF8);
+            if(fsa != null)
+            {
+                fsa.Close();
+            }
+            
+        }
+
+        
+
+    }
+}
---
+++ BasicServerTest/Program.cs
@@ -0,0 +1,265 @@
+﻿using System.Net;
+using System.Threading;
+using System.Text.Json;
+
+// 201727026_정태수
+
+namespace FinalTestSample
+{
+    class data
+    {
+
+    }
+
+    public class Global
+    {
+        public static Mutex mut = new Mutex();    
+        
+        public static List<string> id = new List<string>();
+
+        public static void InitServer()
+        {
+            //서버 초기화 로직을 여기에 담으세요
+
+        }
+
+        public static void ContextHandle(object httplistener)
+        {
+            mut.WaitOne();
+            HttpListener listener = (HttpListener)httplistener;
+            HttpListenerContext context = listener.GetContext();
+            mut.ReleaseMutex();
+
+            //문제 1 : 사용자의 입력을 받고 출력을 한다.
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+
+            if ((request.HttpMethod == "POST") && (request.Url.AbsolutePath == "/Login/"))
+            {
+                int serverID = GetServerID();
+                if (checkLogin(serverID.ToString()))
+                {
+                    id.Add(serverID.ToString());
+                    Message message = new Message();
+                    message.Event = 0;
+                    message.TextMessage = "로그인 성공";
+
+                    string postMsg = JsonSerializer.Serialize(message);
+
+                    byte[] buffers = System.Text.Encoding.UTF8.GetBytes(postMsg);
+                    response.OutputStream.WriteAsync(buffers, 0, buffers.Length);
+
+                    response.OutputStream.Close();
+                }
+            }
+
+            if ((request.HttpMethod == "POST") && (request.Url.AbsolutePath == "/GodDamnChatService/"))
+            {
+                response.AddHeader("Access-Control-Allow-Origin", "*");
+
+                Stream body = request.InputStream;
+                System.Text.Encoding encoding = request.ContentEncoding;
+                System.IO.StreamReader reader = new System.IO.StreamReader(body, encoding);
+
+                string newMessage = JsonSerializer.Deserialize<string>(reader.ReadToEnd());
+
+                Console.WriteLine(newMessage);
+
+                Message message = new Message();
+                message.Event = 3;
+                message.TextMessage = newMessage;
+
+                string postMsg = JsonSerializer.Serialize(message);
+
+                byte[] buffers = System.Text.Encoding.UTF8.GetBytes(postMsg);
+                response.OutputStream.WriteAsync(buffers, 0, buffers.Length);
+
+                response.OutputStream.Close();
+            }
+
+            if ((request.HttpMethod == "POST") && (request.Url.AbsolutePath == "/Logout/"))
+            {
+                Stream body = request.InputStream;
+                System.Text.Encoding encoding = request.ContentEncoding;
+                System.IO.StreamReader reader = new System.IO.StreamReader(body, encoding);
+
+                string newMessage = JsonSerializer.Deserialize<string>(reader.ReadToEnd());
+
+                foreach (string temp in id)
+                {
+                    if(temp == newMessage)
+                    {
+                        id.Remove(temp);
+                    }
+                }
+                Message message = new Message();
+                message.Event = 1;
+                message.TextMessage = "로그아웃";
+
+                string postMsg = JsonSerializer.Serialize(message);
+
+                byte[] buffers = System.Text.Encoding.UTF8.GetBytes(postMsg);
+                response.OutputStream.WriteAsync(buffers, 0, buffers.Length);
+
+                response.OutputStream.Close();
+            }
+
+
+            //---------------------------
+            //여기부턴 예제입니다.
+
+            /*HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+            response.AddHeader("Access-Control-Allow-Origin", "*");
+            Stream body = request.InputStream;
+            System.Text.Encoding encoding = request.ContentEncoding;
+            System.IO.StreamReader reader = new System.IO.StreamReader(body, encoding);
+
+            string rawurl = request.RawUrl;
+            string httpmethod = request.HttpMethod;
+
+            string result = "";
+
+            result += string.Format("httpmethod = {0}\r\n", httpmethod);
+            result += string.Format("rawurl = {0}\r\n", rawurl);
+            System.Console.WriteLine(result);
+
+
+            Message a = new Message();
+            string responseString = JsonSerializer.Serialize(a);
+            System.Console.WriteLine(responseString);
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            response.OutputStream.Write(buffer, 0, buffer.Length);
+            response.OutputStream.Close();*/
+
+            //----------------------------요기까지
+        }
+
+        public static int GetServerID()
+        {
+            //서버에서 고유 id를 부여해서 리턴하시오
+            int serverID = id.Count + 1;
+            return serverID;
+            
+        }
+
+
+        static string GetNewMessage(string pName)
+        {
+            //사용자가 받지못한 최신 메시지를 출력하세요
+
+            return pName;
+        }
+
+        static bool checkLogin(string serverID)
+        {
+            
+            foreach (string s in id)
+            {
+                if(s == serverID)
+                {
+                    return false;
+                }
+            }
+            return true;
+            //사용자ID가 서버에 저장되어있는지 확인하세요.
+            //저장되어있다면 true 아니라면 false
+
+        }
+    }
+
+    public class ServerLoop
+    {
+        HttpListener _listener;
+        string _address;
+        public static Mutex mut = new Mutex();
+
+        public ServerLoop(string address, string port)
+        {
+            RestartLoop();
+            //문제2 :_address에 localhost 주소와 port가 입력된 주소를 작성하시오
+            _address = address + ":" + port;
+
+        }
+
+        public void RestartLoop()
+        {
+            _listener = new HttpListener();
+        }
+
+        
+
+        public void AddRestAPI(string apiName)
+        {
+            //문제3 :address 뒤에 apiName이 추가된 prefix를 추가하세요
+            _listener.Prefixes.Add(_address + "/" + apiName + "/");
+
+        }
+        public void Run()
+        {
+            _listener.Start();
+            Console.WriteLine("Listening...");
+
+
+            while (true)
+            {
+
+                ///문제4: 응답이 들어올경우 mutex를 이용하여 아래 코드를 새로운 접속이 생길때마다 1개의 Thread가 생기도록 개선하시오
+                //Thread t = new Thread(new ParameterizedThreadStart(Global.ContextHandle));
+                // t.Start(_listener);
+
+                // HttpListenerContext ctx = _listener.GetContext();
+                mut.WaitOne();
+                Thread t = new Thread(new ParameterizedThreadStart(Global.ContextHandle));
+                t.Start(_listener);
+                mut.ReleaseMutex();
+            }
+        }
+
+        public void Close()
+        {
+            _listener.Stop();
+        }
+
+       
+
+        void EndLoop()
+        {
+            if (_listener != null)
+            {
+                _listener.Stop();
+                _listener.Close();
+            }
+        }
+    }
+
+
+
+    public class Program
+    {
+        public static int Main()
+        {
+            FileLogger fLog = new FileLogger("Log");
+
+            Global.InitServer();
+
+            //Log type 종류
+            //<Exception>
+            //<Debug>
+            //<Record>
+            //
+            fLog.Write("Debug","I hate life1111.");
+
+            ServerLoop server = new ServerLoop("http://127.0.0.1", "3000");
+            server.AddRestAPI("GodDamnChatService");
+            server.AddRestAPI("Login");
+            server.AddRestAPI("Logout");
+
+            server.Run();
+            server.Close();
+
+
+            return 0;
+        }
+    }
+}
